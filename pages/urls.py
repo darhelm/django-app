0b5f2fbe48944047a9345b498ff455ecb5a757d6
darhelm <mehrad_hadizadeh@yahoo.com>
2023-02-27 14:02:15 +0330

    second commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,6 @@
-from .views import response
+from .views import HomePageView, AboutPageView, BaseView
 from django.urls import path
 
-urlpatterns = [path('', response, name = "home"),]
+urlpatterns = [path('',BaseView.as_view()),
+               path('home/', HomePageView.as_view() , name = "home"),
+               path('about/',AboutPageView.as_view(), name = "about")]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def response(request):
-    html = 'kirekhar'
-    return HttpResponse(html)
+class BaseView(TemplateView):
+    template_name = "base.html"
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
